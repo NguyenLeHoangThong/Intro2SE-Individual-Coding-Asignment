e59545efd7300de3efd4a16623f7039efe66e7df
ThongNguyen <nguyenlehoangthong2002@gmail.com>
2022-11-11 21:49:00 +0700

    Catch event press number, DEL, AC buttons

--- src/Components/Core/Calculator/index.js
+++ src/Components/Core/Calculator/index.js
@@ -7,31 +7,51 @@ import { Button } from 'Components/Common/Button';
 import { ButtonContainer } from 'Components/Common/ButtonContainer';
 
 export default function Calculator() {
+
+  const [currentValue, setCurrentValue] = useState("");
+  const [previousValue, setPreviousValue] = useState("");
+  const [operationValue, setOperationValue] = useState("");
+
+  const handleClick = (data) => {
+    if (data === "." && currentValue.includes(".")) return;
+    setCurrentValue((prev) => prev + data);
+  }
+
+  const handleAC = () => {
+    setCurrentValue("");
+    setPreviousValue("");
+    setOperationValue("");
+  }
+
+  const handleDEL = () => {
+    setCurrentValue(String(currentValue).slice(0, -1));
+  }
+
   return (
     <div>
         <Container>
           <Screen>
-              <Current>10</Current>
-              <Previous>10 +</Previous>
+              <Current>{currentValue}</Current>
+              <Previous>{previousValue} {operationValue}</Previous>
           </Screen>
           <ButtonContainer>
-            <Button spanTwoColumn control>AC</Button>
-            <Button>DEL</Button>
+            <Button spanTwoColumn control onClick={() => handleAC()}>AC</Button>
+            <Button onClick={() => handleDEL()}>DEL</Button>
             <Button operation>%</Button>
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
+            <Button onClick={() => handleClick("7")}>7</Button>
+            <Button onClick={() => handleClick("8")}>8</Button>
+            <Button onClick={() => handleClick("9")}>9</Button>
             <Button operation>X</Button>
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
+            <Button onClick={() => handleClick("4")}>4</Button>
+            <Button onClick={() => handleClick("5")}>5</Button>
+            <Button onClick={() => handleClick("6")}>6</Button>
             <Button operation>+</Button>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
+            <Button onClick={() => handleClick("1")}>1</Button>
+            <Button onClick={() => handleClick("2")}>2</Button>
+            <Button onClick={() => handleClick("3")}>3</Button>
             <Button operation>-</Button>
-            <Button>.</Button>
-            <Button>0</Button>
+            <Button onClick={() => handleClick(".")}>.</Button>
+            <Button onClick={() => handleClick("0")}>0</Button>
             <Button spanTwoColumn operation>=</Button>
           </ButtonContainer>
         </Container>
