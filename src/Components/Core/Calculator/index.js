36632da627c8e6b09528be387754e18da42d08ed
ThongNguyen <nguyenlehoangthong2002@gmail.com>
2022-11-11 22:22:31 +0700

    Update: Key press event for calculator

--- src/Components/Core/Calculator/index.js
+++ src/Components/Core/Calculator/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Container } from 'Components/Common/Container';
 import { Screen } from 'Components/Common/Screen';
 import { Current } from 'Components/Common/Current';
@@ -11,6 +11,70 @@ export default function Calculator() {
   const [currentValue, setCurrentValue] = useState("");
   const [previousValue, setPreviousValue] = useState("");
   const [operationValue, setOperationValue] = useState("");
+
+  useEffect(() => {
+    const handlePressButton = (event) => {
+      event.preventDefault();
+      switch (event?.key) {
+        case "/":
+          handleClickOperator("%");
+          break;
+        case "*":
+          handleClickOperator("X");
+          break;
+        case "+":
+          handleClickOperator("+");
+          break;
+        case "-":
+          handleClickOperator("-");
+          break;
+        case "Enter":
+          handleClickCompute();
+          break;
+        case "Delete":
+          handleDEL();
+          break;
+        case "1":
+          handleClickNumber("1");
+          break;
+        case "2":
+          handleClickNumber("2");
+          break;
+        case "3":
+          handleClickNumber("3");
+          break;
+        case "4":
+          handleClickNumber("4");
+          break;
+        case "5":
+          handleClickNumber("5");
+          break;
+        case "6":
+          handleClickNumber("6");
+          break;
+        case "7":
+          handleClickNumber("7");
+          break;
+        case "8":
+          handleClickNumber("8");
+          break;
+        case "9":
+          handleClickNumber("9");
+          break;
+        case ".":
+          handleClickNumber(".");
+          break;
+        default:
+          return;
+      }
+    }
+      
+    document.addEventListener("keypress", handlePressButton);
+
+    return () => {
+      document.removeEventListener("keypress", handlePressButton)
+    }
+  })
 
   const handleClickNumber = (data) => {
     if (data === "." && String(currentValue).includes(".")) return;
@@ -52,12 +116,10 @@ export default function Calculator() {
 
   const handleClickOperator = (data) => {
     if (currentValue === "") return;
-    
     if (previousValue !== "") {
       setPreviousValue(compute());
-    } 
-    else 
-    {
+    }
+    else {
       setPreviousValue(currentValue);
     }
     setCurrentValue("");
